0874a248f48258e4975f2015466ae8adb7d63e71
Shreyash - Shukla <s.shreyashxp@gmail.com>
2026-03-19 04:18:24 +0530

    Implement new authentication system with better-auth, including login, registration, TRPC integration, and database schema.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,22 @@
-import { getQueryClient, trpc } from "@/trpc/server";
-import { Client } from "./client";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
 
 
+import { authClient } from "@/lib/auth-client";
+import { requireAuth } from "@/lib/auth-utils";
+import { caller } from "@/trpc/server";
+import Logout from "@/app/Logout";
 
-const page = async () =>{
- 
-const queryClient = getQueryClient();
+const page =  async () =>{
 
-void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
+ await requireAuth();
 
+ const data = await caller.getUsers();
 return (
-  <div className="min-h-screen min-w-screen flex items-center justify-center">
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Client/>
-      </Suspense>
-    </HydrationBoundary>
+  <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
+    protected server component
+    <div className="border border-gray-300 p-4">
+      {JSON.stringify(data , null , 2)}
+    </div>
+    <Logout/>
   </div>
 )
 }
