6c6bb029e17ceb054e4f1c67b3b1a68edab6f102
Shreyash - Shukla <s.shreyashxp@gmail.com>
2026-03-20 20:26:05 +0530

    Implement Sentry for comprehensive error monitoring and integrate AI SDK with Google Gemini.

---
+++ sentry.server.config.ts
@@ -0,0 +1,27 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://8adf72fc55b475a36af546104bdb4434@o4511077195841536.ingest.us.sentry.io/4511077215174656",
+
+  integrations: [
+    Sentry.vercelAIIntegration({
+      recordInputs: true,
+      recordOutputs: true,
+    }),
+    Sentry.consoleLoggingIntegration({levels: ["log" , "warn" , "error"]}),
+  ],
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+  sendDefaultPii: true,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
