2e49ae20945220c424c1779cf066ee66eac4a4c7
Shreyash - Shukla <s.shreyashxp@gmail.com>
2026-03-26 00:04:43 +0530

    Implement Inngest-based workflow execution with manual trigger and HTTP request nodes, including real-time status updates.

--- src/features/executions/components/http-request/node.tsx
+++ src/features/executions/components/http-request/node.tsx
@@ -5,6 +5,9 @@ import { GlobeIcon } from "lucide-react";
 import { memo, useState } from "react";
 import { BaseExecutionNode } from "../base-execution-node";
 import { HttpRequestDialog, HTTPRequestFormValues } from "./dialog";
+import { useNodeStatus } from "../../hooks/use-node-status";
+import { HTTP_REQUEST_CHANNEL_NAME, httpRequestChannel } from "@/inngest/channels/http-request";
+import { fetchHttpRequestRealtimeToken } from "./actions";
 
 type HttpRequestNodeData = {
     variableName?: string;
@@ -21,7 +24,12 @@ export const HttpRequestNode = memo((props: NodeProps<HttpRequestNodeType>) => {
     const [dialogOpen , setDialogOpen] = useState(false);
     const { setNodes } = useReactFlow();
 
-    const nodeStatus = "initial";
+    const nodeStatus = useNodeStatus({
+        nodeId: props.id,
+        channel: HTTP_REQUEST_CHANNEL_NAME,
+        topic: "status",
+        refreshToken: fetchHttpRequestRealtimeToken,
+    });
 
     const handleSubmit = (values: HTTPRequestFormValues) => {
         setNodes((prevNodes) =>
